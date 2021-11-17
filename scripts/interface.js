5ccde9f0ca6df465adf598292c7362f60a5c442d
Robert Madureira <robeet73@gmail.com>
2021-11-16 21:11:56 -0300

    fist commit

---
+++ scripts/interface.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    let squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+        square.addEventListener('click', handleClick);
+    })
+
+})
+
+function handleClick(event) {
+    let square = event.target;
+    let position = square.id;
+
+    if (handleMove(position)) {
+
+        setTimeout(() => {
+            alert("O JOGO ACABOU - O Vencedor foi " + playerTime);
+        }, 10);
+    };
+    updateSquares(position);
+}
+
+function updateSquares(position){
+    let square = document.getElementById(position.toString());
+    let symbol = board[position];
+    square.innerHTML = `<div class='${symbol}'></div>`
+}
